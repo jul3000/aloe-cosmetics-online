6bd27c688f634dc52e10385ce512f95de1361665
Julia Egert <julia.egert@yahoo.com>
2024-01-19 12:33:42 +0100

    new button added

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,5 +1,8 @@
-function Buttons ({filteredProducts}) {
+import { data } from './data';
+
+function Buttons ({filteredProducts, setProducts}) {
     return (<div className="cont">
+        <button className="change" onClick={() => setProducts(data)} >All</button>
         <button className="change" onClick={() => filteredProducts ("Body care")} >Body care</button>
         <button className="change" onClick={() => filteredProducts ("Face care")}>Face care</button>
         <button className="change" onClick={() => filteredProducts ("Teeth")}>Teeth</button>
