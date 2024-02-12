57946717dd1800f167329054f19e7d55576fc6af
Julia Egert <137058803+jul3000@users.noreply.github.com>
2024-02-12 15:54:00 +0100

    Update and rename src/Carousel.js to slider.js

--- slider.js
+++ slider.js
@@ -1,36 +1,25 @@
 import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
-function Carousel() {
+function slider() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img 
-      className="d-block w-100"
-      src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" 
-      text="First slide"         
-        />
+        <ExampleCarouselImage text="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img 
-      className="d-block w-100"
-      src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" 
-      text="First slide"         
-        />        
+        <ExampleCarouselImage text="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img 
-      className="d-block w-100"
-      src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" 
-      text="First slide"         
-        />        
+        <ExampleCarouselImage text="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -42,4 +31,4 @@ function Carousel() {
   );
 }
 
-export default Carousel;
+export default slider;
