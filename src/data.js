0f3660e4cabe6fe2d5b222f955e53a423ed8f478
Julia Egert <137058803+jul3000@users.noreply.github.com>
2024-02-12 11:38:09 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,16 +3,15 @@ export const data = [
 
     {
       id: 1,
-      name: "Aloe vera hand wash gel",
+      name: "ALOE VERA HAND WASH GEL",
       searchTerm: "Body care",
       price: 5,
-      content: "Aloe vera hand wash gel",
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/aloe vera soap.jpg?v=1704899763596",
     },
   
     {
       id: 2,
-      name: "Aloe vera face care set",
+      name: "ALOE VERA FACE CARE SET",
       searchTerm: "Face care",
       price: 25,
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/aloe vera face care set.jpg?v=1704900436218"
@@ -20,273 +19,158 @@ export const data = [
   
   
     {
-  
       id: 3,
-  
-      name: "Aloe vera toothpaste",
-  
+      name: "ALOE VERA TOOTHPASTE",
       searchTerm: "Teeth",
-  
       price: 6,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera toothpaste.jpg?v=1704900812822"
-  
     },
   
   
     {
-  
       id: 4,
-  
-      name: "Aloe vera body moisturising gel",
-  
+      name: "ALOE VERA BODY MOISTURISING GEL",
       searchTerm: "Body care",
-  
       price: 7.5,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera moisturising gel.jpg?v=1704901149818"
-  
     },
   
   
     {
-  
       id: 5,
-  
-      name: "Aloe vera sun protection milk SPF 30",
-  
+      name: "ALOE VERA SUN PROTECTION MILK SPF30",
       searchTerm: "Sun care",
-  
       price: 15,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera sun protection milk SPF 30.jpg?v=1704901456946"
-  
     },
   
     {
-  
       id: 6,
-  
-      name: "Aloe vera mouthwash",
-  
+      name: "ALOE VERA MOUTHWASH",
       searchTerm: "Teeth",
-  
       price: 4.95,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera mouthwash.jpg?v=1704901630382"
-  
     },
   
     {
-  
       id: 7,
-  
-      name: "Aloe vera shampoo",
-  
+      name: "ALOE VERA SHAMPOO",
       searchTerm: "Hair",
-  
       price: 7.5,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera shampoo.jpg?v=1704901871265"
-  
     },
   
     {
-  
       id: 8,
-  
-      name: "Aloe vera eye gel",
-  
+      name: "ALOE VERA EYE GEL",
       searchTerm: "Face care",
-  
       price: 6.5,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera eye gel.jpg?v=1704901977074"
-  
     },
   
     {
-  
       id: 9,
-  
-      name: "Aloe vera lip cream SPF6",
-  
+      name: "ALOE VERA LIP CREAM SPF6",
       searchTerm: "Face care",
-  
       price: 3.2,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera lip cream SPF 6.jpg?v=1704902073798"
-  
     },
   
     {
-  
       id: 10,
-  
-      name: "Aloe vera facial tonic",
-  
+      name: "ALOE VERA FACIAL TONIC",
       searchTerm: "Face care",
-  
       price: 7.5,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera facial tonic.jpg?v=1704902174303"
-  
     },
   
     {
-  
       id: 11,
-  
-      name: "Aloe vera skin protection cream",
-  
+      name: "ALOE VERA SKIN PROTECTION CREAM",
       searchTerm: "Face care",
-  
       price: 6.55,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera skin protection cream.jpg?v=1704925909296"
-  
     },
   
     {
-  
       id: 12,
-  
-      name: "Aloe vera face cleansing gel",
-  
+      name: "ALOE VERA FACE CLEANSING GEL",
       searchTerm: "Face care",
-  
       price: 6.95,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera face cleansing gel.jpg?v=1704926023062"
-  
     },
   
     {
-  
       id: 13,
-  
-      name: "Aloe vera sun protection milk SPF20",
-  
+      name: "ALOE VERA SUN PROTECION MILK SPF20",
       searchTerm: "Sun care",
-  
       price: 13.6,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera sun protection milk SPF20.jpg?v=1704926103057"
-  
     },
   
     {
-  
       id: 14,
-  
-      name: "Aloe vera face moisturing ampoules 14* 2 ml",
-  
+      name: "ALOE VERA FACE MOISTURING AMPOULES 14 * 2 ml",
       searchTerm: "Face care",
-  
       price: 19,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera face moisturing ampoules.jpg?v=1704926209476"
-  
     },
   
     {
-  
       id: 15,
-  
-      name: "Aloe vera welness bath",
-  
+      name: "ALOE VERA WELNESS BATH",
       searchTerm: "Body care",
-  
       price: 9.5,
-  
       image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera welness bath.jpg?v=1704926349729"
-  
     },
 
     {
-  
         id: 16,
-    
-        name: "Queen's propolis ampoules",
-    
+        name: "QUEEN'S PROPOLIS AMPOULES",
         searchTerm: "Face care",
-    
         price: 29.75,
-    
         image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Queen's propolis facial ampoules.jpg?v=1704926837064"
-    
       },
 
       {
-  
         id: 17,
-    
-        name: "Aloe vera facial scrub",
-    
+        name: "ALOE VERA FACIAL SCRUB",
         searchTerm: "Face care",
-    
         price: 7.5,
-    
         image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera facial scrub.jpg?v=1704926484291"
-    
       },
 
       {
-  
         id: 18,
-    
-        name: "Queen's propolis facial set",
-    
+        name: "QUEEN'S PROPOLIS FACIAL SET",
         searchTerm: "Face care",
-    
         price: 55,
-    
         image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera queens' propolis facial set.jpg?v=1704926623658"
-    
       },
 
       {
-  
         id: 19,
-    
-        name: "Aloe vera shower gel",
-    
+        name: "ALOE VERA SHOWER GEL",
         searchTerm: "Body care",
-    
         price: 8,
-    
         image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe vera shower gel.jpg?v=1704927023203"
-    
       },
 
       {
-  
         id: 20,
-    
-        name: "Queen's propolis cream",
-    
+        name: "QUEEN'S PROPOLIS CREAM",
         searchTerm: "Face care",
-    
         price: 35.2,
-    
         image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Queen's propolis cream.jpg?v=1704927116944"
-    
       },
 
       {
-  
         id: 21,
-    
-        name: "Aloe vera body spray",
-    
+        name: "ALOE VERA BODY SPRAY",
         searchTerm: "Body care",
-    
         price: 6.55,
-    
         image: "https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Aloe%20vera%20body%20spray.png?v=1704927332597"
-    
       }
-  
   ]
 
   export default data;
