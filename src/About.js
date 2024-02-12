c65999784b4a092e4a85612c896106838e2f70c5
Julia Egert <137058803+jul3000@users.noreply.github.com>
2024-02-12 12:50:00 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -18,20 +18,20 @@ function About() {
                 <p className="company-name" ref={twoRef}>by Sanct Bernhard</p>
             </div>    
         </div>
-        
-        <div className="about-pictures-container">
-        <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" alt="company" width="340px"/>
-        <br/>
-        <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/garden.jpg?v=1705060547265" alt="company" width="340px"/>
-        <br/>
-        <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/office%202.jpg?v=1705060569039" alt="company" width="340px"/>
-        </div>
-        
+
         <div>
             <h1>About Sanct Bernhard</h1>
             <p className="company-name">Tradition since 1903</p>
             <p>Since 1903, the name Sanct Bernhard has stood for high-quality, effective natural remedies, nutritional supplements and personal care products from our own production. Our long experience, qualified employees, state-of-the-art production processes and meticulous controls ensure that all our products meet the highest standards.</p>
             <p>Wherever possible and expedient, our raw materials are of vegetable or mineral origin. We use carefully selected, highly dosed botanical essences, essential oils and active ingredients from nature that give you what you need for natural health, beauty and well-being.</p>    
+        </div> 
+        
+        <div className="about-pictures-container">
+            <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" alt="company" width="340px"/>
+            <br/>
+            <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/garden.jpg?v=1705060547265" alt="company" width="340px"/>
+            <br/>
+            <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/office%202.jpg?v=1705060569039" alt="company" width="340px"/>
         </div>
     </div>)
 }
