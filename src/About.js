453d35e91bf81a22813a1b3bb11af4fd41ad78e1
Julia Egert <137058803+jul3000@users.noreply.github.com>
2024-02-12 15:29:43 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -24,6 +24,8 @@ function About() {
             <p>Since 1903, the name Sanct Bernhard has stood for high-quality, effective natural remedies, nutritional supplements and personal care products from our own production. Our long experience, qualified employees, state-of-the-art production processes and meticulous controls ensure that all our products meet the highest standards.</p>
             <p>Wherever possible and expedient, our raw materials are of vegetable or mineral origin. We use carefully selected, highly dosed botanical essences, essential oils and active ingredients from nature that give you what you need for natural health, beauty and well-being.</p>    
         </div> 
+
+        <button>Click me</button>
         
         <div className="about-pictures-container">
             <img className="about-picture" src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" alt="company" width="340px"/>
