4827f8f846093b999b9718c5ed88b043cfb5785b
Julia Egert <137058803+jul3000@users.noreply.github.com>
2024-02-12 15:47:39 +0100

    Update Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,21 +4,33 @@ function Carousel() {
   return (
     <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <img 
+      className="d-block w-100"
+      src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" 
+      text="First slide"         
+        />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <img 
+      className="d-block w-100"
+      src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" 
+      text="First slide"         
+        />        
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img 
+      className="d-block w-100"
+      src="https://cdn.glitch.global/ac9d64dd-b114-43b4-925f-496de557b57e/Sanct%20Bernhard.jpg?v=1705056044742" 
+      text="First slide"         
+        />        
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
